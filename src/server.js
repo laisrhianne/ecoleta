aea7de3e02f009e99c4177929cefe0919d3aab26
laisrhianne <lays.rhianne@gmail.com>
2020-06-10 07:48:02 -0300

    index route added

--- src/server.js
+++ src/server.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const server = express();
 
+// Configurar pasta pública
+server.use(express.static('public'));
+
+// Configurar caminhos da aplicação
+// Página inicial
+server.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
 // Ligar o servidor
 server.listen(3000);
